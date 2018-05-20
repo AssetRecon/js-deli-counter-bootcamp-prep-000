38ad612fbbbec76abb4831a0a158ca60b023fd10
Edsel Caprice <caprice@mail.usf.edu>
2018-05-20 02:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function takeANumber (katzDeliLine,name) {
 function nowServing(katzDeliLine) {
 if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!";
-  } else {
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
   }
+  
+  console.log(`Currently serving ${katzDeliLine[0]}.`);
+  
  katzDeliLine.shift();
  return katzDeliLine;
 }
